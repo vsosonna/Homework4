fb85e62a3f5a4d19905e96be7d7775bf03a40a80
vsosonna <vsosonna@gmail.com>
2023-07-13 10:13:01 +0200

    первый комит

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+       // task2();
+       // task3();
+       // task4();
+       // task5();
+       // task6();
+        //task7();
+
+    }
+
+    public static void task1() {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("он совершеннолетний");
+        } else {
+            System.out.println("он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+
+
+}
